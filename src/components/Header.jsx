37273ece9a5ac70f97a9058791e6a9bc6fe3a05a
Drubo Roy Bishwas <doctorhackan@gmail.com>
2025-11-05 21:27:00 +0600

    created header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+
+    return (
+        <div className="container mx-auto text-center my-4">
+            <h1 className="text-3xl font-bold">Welcome to Knowledge Cafe</h1>
+        </div>
+    );
+
+}
